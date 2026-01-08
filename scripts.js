640c3c82a3db2ba71b4ab73deb4e12cb805b1bb8
Divasdev <sharmadivas881@gmail.com>
2026-01-08 18:58:51 +0530

    writing javascript

--- scripts.js
+++ scripts.js
@@ -1,9 +1,40 @@
+let questionsData=[];
+let currentQuestionsIndex=0;
+
 async function fetchQuestions(){
    const response = await fetch("https://opentdb.com/api.php?amount=12&category=19&difficulty=medium&type=multiple");
    const data= await response.json();
 
-   console.log(data)
+    questionsData=data.results;
+
+   console.log(questionsData)
+
+   showQuestions();
 }
 
 
 fetchQuestions();
+
+function showQuestions(){
+   questions=questionsData[currentQuestionsIndex];
+   console.log(questions);
+
+
+   indexedquestion=questions.question
+   indexedCorrectAnswer=questions.correct_answer;
+   indexedIncorrectAnswer=questions.incorrect_answers;
+
+    options=[indexedCorrectAnswer,indexedIncorrectAnswer];
+   shuffle(options);
+    console.log(options);
+
+   
+
+   const displayQuestion=document.querySelector(".question");
+
+   displayQuestion.innerHTML= indexedquestion;
+   
+
+
+
+}
